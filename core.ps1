9451754f6821c50acf5d976696279a10712641f6
Sergey Stepanenko <powersa@mail.ru>
2022-04-18 19:08:39 +0300

    turn off output to standart

--- core.ps1
+++ core.ps1
@@ -134,8 +134,8 @@ function Start-ModifyFileAsync
     # set process param
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     $pinfo.FileName = $exePath
-    $pinfo.RedirectStandardError = $true
-    $pinfo.RedirectStandardOutput = $true
+    $pinfo.RedirectStandardError = $false
+    $pinfo.RedirectStandardOutput = $false
     $pinfo.UseShellExecute = $false
     $pinfo.WindowStyle = [System.Diagnostics.ProcessWindowStyle]::Hidden
     # hide window
